7fd65e924b377281410ba17eca7b825d0c7cfa28
soichiro <sou412snow@gmail.com>
2020-05-24 19:24:03 -0400

    add functions

---
+++ R/msmFE.R
@@ -0,0 +1,34 @@
+#' Main function  for estimating treatment effects
+#' @export
+msmFE <- function(
+  formula_outcome = NULL,
+  formula_treatment,
+  data, index,
+  estimator = "HT",
+  fixed_effects = TRUE,
+  options = list()
+) {
+
+
+  ## data format
+  ## remove no-variation observations if
+}
+
+
+
+#' Function to estimate propensity scores
+#' @export
+estimate_pscore <- function(formula, data) {
+
+  ## estimate
+  fit <- bife::bife(formula, data = data, model = "logit")
+
+  ## propensity score
+  fitted <- predict(fit, type = "response")
+  return(list(fit = fit, ps = fitted))
+}
+
+
+estimate_outcome <- function() {
+
+}
